24e25beb8f85378685e45c505ed869e28d543909
wahm <wahmalghaili@gmail.com>
2025-11-20 19:41:08 +0300

    i dont no

---
+++ src/Array/Main.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+ int []number={1,2,3,4,5};
+ //int []number2=new int[5];
+ //System.out.printf(Arrays.toString(number));
+for (int i=0;i<number.length;i++)
+    //System.out.printf(number[i]);
+}
+
+
+}
